2293bf7a1a3b22fbfc9ab02b17428e705c0113fb
Himanshu Bhirud <ythimanshu400@gmail.com>
2026-03-22 20:01:10 +0530

    Create calculator.js

---
+++ Basics/calculator.js
@@ -0,0 +1,9 @@
+let a = 10, b = 5;
+let op = '+';
+
+switch (op) {
+  case '+': console.log(a + b); break;
+  case '-': console.log(a - b); break;
+  case '*': console.log(a * b); break;
+  case '/': console.log(a / b); break;
+}
