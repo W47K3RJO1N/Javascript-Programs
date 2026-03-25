52a9805ba67213d19895c968340e502d52e210c0
Himanshu Bhirud <ythimanshu400@gmail.com>
2026-03-25 22:47:50 +0530

    Create sotingarr.js

---
+++ Arrays/sotingarr.js
@@ -0,0 +1,11 @@
+// for ascending sorting
+
+let arr = [5, 2, 9, 1];
+
+arr.sort();
+
+console.log(arr); 
+
+// to sort for descending order the following can be followed
+
+arr.sort((a, b) => b - a);
