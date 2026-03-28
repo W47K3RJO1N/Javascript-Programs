94a335e8fd605dc115d597f3b41ef4c29578e45c
Himanshu Bhirud <ythimanshu400@gmail.com>
2026-03-28 23:06:58 +0530

    Create arrsorted.js

---
+++ Arrays/arrsorted.js
@@ -0,0 +1,11 @@
+let arr = [1, 2, 3, 4, 5];
+let isSorted = true;
+
+for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+        isSorted = false;
+        break;
+    }
+}
+
+console.log(isSorted ? "Sorted" : "Not Sorted");
