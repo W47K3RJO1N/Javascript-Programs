adf87e6e80d3dd6cf417b430e034f25d0339bf24
Himanshu Bhirud <ythimanshu400@gmail.com>
2026-03-28 23:07:49 +0530

    Create freqarr.js

---
+++ Arrays/freqarr.js
@@ -0,0 +1,13 @@
+let arr = [1, 2, 2, 3, 3, 3];
+
+let freq = {};
+
+for (let i = 0; i < arr.length; i++) {
+    if (freq[arr[i]]) {
+        freq[arr[i]]++;
+    } else {
+        freq[arr[i]] = 1;
+    }
+}
+
+console.log(freq);
